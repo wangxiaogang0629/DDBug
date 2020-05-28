0f79f8804c914a90dd02ea352cc5ff8921f0382a
wangxiaogang <wangxiaogang@macdeMacBook-Pro.local>
2020-05-28 13:48:15 +0800

    ddbug反馈

---
+++ demo/.tea/entryFiles-production/index$.web.js
@@ -0,0 +1,7 @@
+require('@alipay/appx-compiler/lib/sjsEnvInit');
+require('./config$');
+
+require('../../pages/index/index?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/second/second?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/three/three?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/four/four?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
